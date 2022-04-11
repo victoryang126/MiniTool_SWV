88218c3b82078d83ad140c3237708850d60cbd64
Victoryang126 <tiddUq-mymji4-buvgyg>
2022-04-11 22:52:58 +0800

    add button updload report directly

--- CB_Spec_Tool/__init__.py
+++ CB_Spec_Tool/__init__.py
@@ -2,4 +2,4 @@ __all__ = ["CB_Spec_Tool_Widget","ConvertSpect2CBSpec","Ui_CB_Spec_Tool","Upload
 
 from CB_Spec_Tool import  *
 from CB_Spec_Tool.CB_Spec_Tool_Widget import *
-from CB_Spec_Tool.UploadSpec2CodeBeamer import *
+from UploadSpec2CodeBeamer import *
